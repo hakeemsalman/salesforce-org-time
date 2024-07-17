744284b55e5d5a95cbde1c1c9d4f3ff2a0a9364b
Salman Hakeem <salman74163@gmail.com>
2024-07-18 01:34:02 +0530

    v1.0

---
+++ background.js
@@ -0,0 +1,45 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // Perform cookie operations in the background page, because not all foreground pages have access to the cookie API.
+  // Firefox does not support incognito split mode, so we use sender.tab.cookieStoreId to select the right cookie store.
+  // Chrome does not support sender.tab.cookieStoreId, which means it is undefined, and we end up using the default cookie store according to incognito split mode.
+  if (request.message == "getSfHost") {
+    const currentDomain = new URL(request.url).hostname;
+    // When on a *.visual.force.com page, the session in the cookie does not have API access,
+    // so we read the corresponding session from *.salesforce.com page.
+    // The first part of the session cookie is the OrgID,
+    // which we use as key to support being logged in to multiple orgs at once.
+    // http://salesforce.stackexchange.com/questions/23277/different-session-ids-in-different-contexts
+    // There is no straight forward way to unambiguously understand if the user authenticated against salesforce.com or cloudforce.com
+    // (and thereby the domain of the relevant cookie) cookie domains are therefore tried in sequence.
+    chrome.cookies.get({url: request.url, name: "sid", storeId: sender.tab.cookieStoreId}, cookie => {
+      if (!cookie || currentDomain.endsWith(".mcas.ms")) { //Domain used by Microsoft Defender for Cloud Apps, where sid exists but cannot be read
+        sendResponse(currentDomain);
+        return;
+      }
+      const [orgId] = cookie.value.split("!");
+      const orderedDomains = ["salesforce.com", "cloudforce.com", "salesforce.mil", "cloudforce.mil", "sfcrmproducts.cn"];
+      orderedDomains.forEach(currentDomain => {
+        chrome.cookies.getAll({name: "sid", domain: currentDomain, secure: true, storeId: sender.tab.cookieStoreId}, cookies => {
+          let sessionCookie = cookies.find(c => c.value.startsWith(orgId + "!"));
+          if (sessionCookie) {
+            sendResponse(sessionCookie.domain);
+          }
+        });
+      });
+    });
+    return true; // Tell Chrome that we want to call sendResponse asynchronously.
+  }
+  if (request.message == "getSession") {
+    sfHost = request.sfHost;
+    chrome.cookies.get({url: "https://" + request.sfHost, name: "sid", storeId: sender.tab.cookieStoreId}, sessionCookie => {
+      if (!sessionCookie) {
+        sendResponse(null);
+        return;
+      }
+      let session = {key: sessionCookie.value, hostname: sessionCookie.domain};
+      sendResponse(session);
+    });
+    return true; // Tell Chrome that we want to call sendResponse asynchronously.
+  }
+  return false;
+});
---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict"
+
+let sfHosted;
+let sid;
+let orgTime;
+
+const getTime = async ()  =>{
+  let dt = new Date();
+  dt = dt.toLocaleString("en-US", { timeZone: `${orgTime}` });
+  return dt;
+}
+
+function createTime() {
+  let h1 = document.querySelector('#dt-inspect');
+  if (!h1) {
+    let bd = document.querySelector('body');
+    let orgTimeSlot = document.createElement('div');
+    orgTimeSlot.setAttribute('id', 'dt-inspect');
+    orgTimeSlot.classList.add('slds-button','slds-button_outline-brand','slds-text-title_bold'); // Add the class name
+
+    // Set styles
+    orgTimeSlot.style.zIndex = '1000';
+    orgTimeSlot.style.position = 'fixed';
+    orgTimeSlot.style.top = '2rem';
+    orgTimeSlot.style.right = '-11rem';
+    orgTimeSlot.style.transition = 'right 1s ease-in-out';
+
+    // Handle hover effect
+    orgTimeSlot.addEventListener('mouseover', () => {
+      orgTimeSlot.style.right = '1rem';
+    });
+
+    orgTimeSlot.addEventListener('mouseout', () => {
+      orgTimeSlot.style.right = '-11rem';
+    });
+
+    setInterval(async  () => {
+      orgTimeSlot.innerHTML = `<h5>${await getTime()}</h5>`; // Display the localized date and time
+    }, 1000);
+    bd.appendChild(orgTimeSlot);
+  }
+}
+
+if (document.querySelector("body.sfdcBody, body.ApexCSIPage, #auraLoadingBox") || location.host.endsWith("visualforce.com")) {
+  // We are in a Salesforce org
+  chrome.runtime.sendMessage({ message: "getSfHost", url: location.href }, sfHost => {
+    if (sfHost) {
+      sfHosted = sfHost;
+      chrome.runtime.sendMessage({ message: "getSession", sfHost: sfHost }, async cookie => {
+        sid = cookie.key;
+        await sfConnect();
+        await createTime();
+      })
+    }
+  });
+  
+  async function sfConnect() {
+    await fetch(`https://${sfHosted}/services/data/v60.0/query/?q=SELECT%20TimeZoneSidKey%20FROM%20Organization`, {
+      method: 'GET',
+      headers: {
+        "Authorization": 'Bearer ' + sid,
+        Accept: '*/*',
+        Host: 'peo.my.salesforce.com',
+        'Accept-Encoding': 'gzip, deflate, br',
+      }
+    }).then( async response => {
+      let data = await response.json();
+      orgTime = data.records[0].TimeZoneSidKey;
+    })
+  }
+
+}
